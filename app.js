fff993f772a893d2809b8acf7b1f6d91d2c03b22
Matheus Gomes <54047572+matheusgomes2020@users.noreply.github.com>
2023-03-27 17:05:10 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const express = require("express");
-
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use( bodyParser.urlencoded({extended: true}) )
+
 app.get("/", function(req, res) {
 
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=Campinas&appid={apiKey}&imperial"
+    res.sendFile(__dirname + "/index.html");
+    
+});
+
+app.post("/", function(req, res) {
+
+    const query = req.body.cityName;
+    const apiKey = "******";
+    const unit = "metric";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit
 
     https.get(url, function(response) {
 
@@ -20,7 +31,7 @@ app.get("/", function(req, res) {
             const icon = weatherData.weather[0].icon
             const imageUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
             res.write( "<p>O tempo atualmente está "  + waetherDescription + "<p>" );
-            res.write( "<h1>A temperatura em Campinas é " + temp + " graus.</h1>" );
+            res.write( "<h1>A temperatura em " + query + " é " + temp + " graus.</h1>" );
             res.write("<img src=" + imageUrl + ">");
 
             res.send();
@@ -29,7 +40,8 @@ app.get("/", function(req, res) {
 
     });
 
-});
+})
+
 
 app.listen(3000, function(){
     console.log("Servidor começou na porta 3000.")
